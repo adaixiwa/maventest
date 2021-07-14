7169f59eb10030c4edbeb808ee75efa26ab0b033
yonge <123@qq.com>
2021-07-15 00:09:12 +0800

    第二.1次提交

--- src/main/java/com/yonge/Git.java
+++ src/main/java/com/yonge/Git.java
@@ -4,5 +4,8 @@ public class Git {
 
     public static void main(String[] args) {
         System.out.println("git");
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
     }
 }
