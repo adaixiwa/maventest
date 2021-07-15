a25d5c81c629ad6fc4c767525806a581d78b078f
yonge <123@qq.com>
2021-07-15 09:38:28 +0800

    第5次提交

--- src/main/java/com/yonge/Git.java
+++ src/main/java/com/yonge/Git.java
@@ -3,10 +3,8 @@ package com.yonge;
 public class Git {
 
     public static void main(String[] args) {
-        System.out.println("git");
-        for (int i = 0; i < 100; i++) {
-            System.out.println(i);
-        }
-        System.out.println("asdfasdf");
+        System.out.println("111");
+        System.out.println("222");
+        System.out.println("333");
     }
 }
